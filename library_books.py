73bb915c88bade398e4ff614111ef02daf2bb3d9
Yourdan102895 <ponyong63@gmail.com>
2025-06-20 18:21:24 +0800

    Create library_books.py

---
+++ library_books.py
@@ -0,0 +1,29 @@
+books_by_category = {
+    "Office Administration": [
+        {"title": "Administrative Office Management", "author": "Pattie Odgers", "availability": "Available"},
+        {"title": "Administrative Professional", "author": "Karin M. Stulz", "availability": "Available"},
+    ],
+    "Hospitality Management": [
+        {"title": "Introduction to Hospitality Management", "author": "John R. Walker", "availability": "Available"},
+        {"title": "Hospitality Management and Organisational Behaviour", "author": "Laurie Jay Mullins", "availability": "Available"},
+        {"title": "Accommodations Operations and Management", "author": "author", "availability": "Available"},
+        {"title": "Gastronomy and the Dining Experience", "author": "John R. Walker", "availability": "Available"},
+        {"title": "Food Production Operations", "author": "Parvinder S. Bali", "availability": "Available"},
+    ],
+    "Computer Engineering": [
+        {"title": "Digital Design and Computer Architecture", "author": "David Monley", "availability": "Available"},
+        {"title": "The Hardware/Software Interface", "author": "David Patterson", "availability": "Available"},
+    ],
+    "Information Technology": [
+        {"title": "Learn Python", "author": "Al Sweigart", "availability": "Available"},
+        {"title": "JavaScript for Kids", "author": "Nick Morgan", "availability": "Available"},
+        {"title": "A Cost Based Approach to Project Management", "author": "Mehmet, Nihat, Hanioğlu", "availability": "Available"},
+        {"title": "Advanced Database Management Systems", "author": "author", "availability": "Available"},
+        {"title": "AI-Enabled 6G Networks and Applications", "author": "Gupta Ragab Mansour Khamparia Khanna", "availability": "Available"},
+        {"title": "C++ Programming", "author": "Jones", "availability": "Available"},
+    ],
+    "PE": [
+        {"title": "Sports Science", "author": "Dr. Smith", "availability": "Available"},
+        {"title": "Yoga for All", "author": "Patricia Allen", "availability": "Available"},
+    ],
+}
